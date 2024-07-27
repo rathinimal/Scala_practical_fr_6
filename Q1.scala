ab66d6d722f8dad70667c6e67b7d4385fa30c338
rathinimal <rathinarasanimal@gmail.com>
2024-07-27 21:41:23 +0530

    Q1 is started

---
+++ Q1.scala
@@ -0,0 +1,13 @@
+import scala.collection.mutable
+import scala.io.StdIn.{readLine,readInt}
+
+object inventory_warehouse {
+    def main(args: Array[String]): Unit = {
+        case class product(uniqueId:Int,name:String,quantity:Int,price:Double)
+
+        val inventory: mutable.Map[String, product] = mutable.Map()
+
+    }
+
+    
+}
