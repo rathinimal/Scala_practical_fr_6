ee42af8938f078f69ab097ac4e44221188d8a9ec
rathinimal <rathinarasanimal@gmail.com>
2024-07-28 11:56:29 +0530

    Q1 is completed

--- Q1.scala
+++ Q1.scala
@@ -9,13 +9,13 @@ object inventory_warehouse {
 
         val inventory1 : Map[Int,Detail_of_products] = Map(
             2020 -> ("Product_1", 32,10.00),
-            2021 -> ("Product_2", 20,40.00),
+            2021 -> ("Product_2", 20,100.00),
             2022 -> ("Product_3",10,5.00),
             2023 -> ("Product_4",40,25.00)
         )
 
         val inventory2 : Map[Int,Detail_of_products]=Map(
-            2021 -> ("Product_2", 20,50.00),
+            2021 -> ("Product_2", 30,50.00),
             2025 -> ("Product_54",10,10.00),
         )
 
@@ -25,15 +25,43 @@ object inventory_warehouse {
         }
 
         //Q2 total value of products
-        def total_value(m:Map[Int,Detail_of_products]): Unit ={
-            print(m.values.map{case (_,quantity,price) => quantity* price})
+        def total_value(m:Map[Int,Detail_of_products]): Double ={
+            m.values.map{case (_,quantity,price) => quantity* price}.sum
         }
 
-        def 
+        // def mergeInventory(x:Map[Int,Detail_of_products],y:Map[Int,Detail_of_products]):Map[Int,Detail_of_products] = {
+        //     val mergeInventory1 = x++y
+        //     x match{
+        //         case x  if (x.keys == y.keys) => mergeInventory1.updated()
+        //     }
+            
+        // }
+         def mergeInventory(inv1: Map[Int,Detail_of_products], inv2: Map[Int,Detail_of_products]): Map[Int, Detail_of_products] = {
+            inv2.foldLeft(inv1) { case (mergedInventory, (id, (name, quantity, price))) =>
+                mergedInventory.get(id) match {
+                    case Some((_, existingQuantity, existingPrice)) =>
+                        mergedInventory.updated(id, (name, existingQuantity + quantity, Math.max(existingPrice, price)))
+                    case None =>
+                        mergedInventory + (id -> (name, quantity, price))
+                } 
+            }
+        }
+
+        def checkInventory(inventSearch:Map[Int,Detail_of_products],Id:Int): Unit = {
+            inventSearch.get(Id) match {
+                case Some((name,quantity,price)) => println(s"Product is found and Product details: $Id, Name: $name , Quantity: $quantity , Price : $price")
+                case None => println(s"Product Id is not exist in inventory1")
+            }
+        }
+        
 
         view_products(inventory1)
-        total_value(inventory1)
-    }
+        println(s"The total value of proudcts is : ${total_value(inventory1)}")
+        println(s"The inside contains of inventory1 is ${inventory1.isEmpty}")
+        println(mergeInventory(inventory1,inventory2))
+        checkInventory(inventory1,2022)
+        checkInventory(inventory1,2000)
+    }   
 
     
 }
